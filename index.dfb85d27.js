b4c8afb6796f1c4ffb97f45c938c1de697c67f0b
olegoleg0080 <120270714+olegoleg0080@users.noreply.github.com>
2023-12-22 20:29:07 +0000

    Deploying to gh-pages from @ olegoleg0080/man@38c9aa842d539fbbcac56cfb75a635244ce0b28c 🚀

--- index.dfb85d27.js
+++
@@ -1,2 +0,0 @@
-var refs={modalOpen:document.querySelector(".burger"),burgerModal:document.querySelector(".burger-modal")};function openModal(e){e.classList.contains("hidden")?e.classList.remove("hidden"):e.classList.add("hidden")}refs.modalOpen.addEventListener("click",openModal(refs.burgerModal));
-//# sourceMappingURL=index.dfb85d27.js.map
