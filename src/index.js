1a112a3156df0746debfa6d6283fa7f33eabb79c
olegoleg0080 <120270714+olegoleg0080@users.noreply.github.com>
2024-01-17 09:25:40 +0200

    готово все крім адаптива і селектів

--- src/index.js
+++ src/index.js
@@ -2,9 +2,15 @@ const refs = {
     modalOpen: document.querySelector('.burger'),
     burgerModal: document.querySelector('.burger-modal'),
     modalClose: document.querySelector('.modal__close'),
+    loginModal: document.querySelector('.login'),
+    accountBtn: document.querySelector('.support__btn-account'),
 }
 refs.modalOpen.addEventListener('click', () => {
     refs.burgerModal.classList.remove('hidden');
+})
+refs.accountBtn.addEventListener('click', () => {
+    refs.loginModal.classList.remove('hidden');
+    document.body.style = "overflow: hidden";
 })
 refs.modalClose.addEventListener('click', () => {
     refs.burgerModal.classList.add('hidden');
