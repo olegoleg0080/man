36c520779c014c7b0bc1bad1e60ffc6ef84307c1
olegoleg0080 <120270714+olegoleg0080@users.noreply.github.com>
2024-01-19 17:32:10 +0200

    мрммромр

--- src/index.js
+++ src/index.js
@@ -4,10 +4,16 @@ const refs = {
     modalClose: document.querySelector('.modal__close'),
     loginModal: document.querySelector('.login'),
     accountBtn: document.querySelector('.support__btn-account'),
+    cardElement: document.getElementsByTagName('.card__table-data'),
+    redactContainer: document.querySelector('.redact__container'),
 }
 refs.modalOpen.addEventListener('click', () => {
     refs.burgerModal.classList.remove('hidden');
 })
+// refs.cardElement.addEventListener('click', () => {
+//     refs.redactContainer.classList.remove('hidden');
+//     document.body.style = "overflow: hidden";
+// })
 refs.accountBtn.addEventListener('click', () => {
     refs.loginModal.classList.remove('hidden');
     document.body.style = "overflow: hidden";
