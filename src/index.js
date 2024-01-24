bf505cb854b376e14261c1a47dbc5e921a0d3270
olegoleg0080 <120270714+olegoleg0080@users.noreply.github.com>
2024-01-24 02:51:35 +0200

    js1

--- src/index.js
+++ src/index.js
@@ -7,28 +7,73 @@ const refs = {
   containerScroll: document.querySelector('.container-scroll'),
   redactContainer: document.querySelector('.redact__container'),
   redactClose: document.querySelector('.redact__close'),
+  modalGroup: document.querySelector('.modal-block-group'),
+  modalVac: document.querySelector('.modal-block-vac'),
+  modalBtnGroup: document.querySelector('.sort__group'),
+  modalBtnVac: document.querySelector('.sort__vaccination'),
+  redactBlock: document.querySelector('.redact__blocks'),
 };
-refs.modalOpen.addEventListener('click', () => {
-  refs.burgerModal.classList.remove('hidden');
-});
-refs.accountBtn.addEventListener('click', () => {
-  refs.loginModal.classList.remove('hidden');
-  document.body.style = 'overflow: hidden';
-});
-refs.modalClose.addEventListener('click', () => {
-  refs.burgerModal.classList.add('hidden');
-});
-refs.burgerModal.addEventListener('click', (e) => {
-  if (!e.target.closest('.modal__container')) {
-    refs.burgerModal.classList.add('hidden');
-  }
-});
-refs.containerScroll.addEventListener('click', (e) => {
-    if (e.target.closest('tr')) {
-      refs.redactContainer.classList.remove('hidden');
-      document.body.style = 'overflow: hidden';
+function modalOpen(btnName, modalBlock) {
+  btnName.addEventListener('click', () => {
+    modalBlock.classList.remove('hidden');
+    document.body.style = 'overflow: hidden';
+  });
+}
+function modalClose(btnName, modalBlock) {
+  btnName.addEventListener('click', () => {
+    modalBlock.classList.add('hidden');
+    document.body.style = 'overflow-y: scroll';
+  });
+}
+function modalCloseOut(btnName, className, modalBlock) {
+  //? btnName при нажатті на що буде закриватися модалка
+  //? className буде перевірятися чи не надато на className
+  //? modalBlock те що буде ховатися
+  btnName.addEventListener('click', e => {
+    if (!e.target.closest(className)) {
+      modalBlock.classList.add('hidden');
+      document.body.style = 'overflow: scroll';
     }
   });
-refs.redactClose.addEventListener('click', ()=>{
-    refs.redactContainer.classList.add('hidden');
-})
+}
+function modalRadio2(btnName1, btnName2, modalBlock1, modalBlock2) {
+  btnName1.addEventListener('click', () => {
+    if (modalBlock1.classList.contains('hidden')) {
+      modalBlock1.classList.remove('hidden');
+      modalBlock2.classList.add('hidden');
+    } else {
+      modalBlock1.classList.add('hidden');
+    }
+  });
+  btnName2.addEventListener('click', () => {
+    if (modalBlock2.classList.contains('hidden')) {
+      modalBlock2.classList.remove('hidden');
+      modalBlock1.classList.add('hidden');
+    } else {
+      modalBlock2.classList.add('hidden');
+    }
+  });
+  //? при натисканні одної модалки інша ховається
+}
+modalRadio2(
+  refs.modalBtnGroup,
+  refs.modalBtnVac,
+  refs.modalGroup,
+  refs.modalVac
+);
+modalOpen(refs.modalOpen, refs.burgerModal);
+modalOpen(refs.accountBtn, refs.loginModal);
+modalClose(refs.modalClose, refs.burgerModal);
+modalClose(refs.redactClose, refs.redactContainer);
+modalCloseOut(refs.burgerModal, '.modal__container', refs.burgerModal);
+modalCloseOut(document.body, '.header__sort-modal', refs.modalGroup);
+modalCloseOut(document.body, '.header__sort-modal', refs.modalVac);
+modalCloseOut(refs.redactContainer, '.redact__blocks', refs.redactContainer);
+// ! функції які використовуються лише 1 раз або просто геморні функціх до яких я ше не дійшов
+refs.containerScroll.addEventListener('click', e => {
+  if (e.target.closest('tr')) {
+    refs.redactContainer.classList.remove('hidden');
+    document.body.style = 'overflow: hidden';
+    console.log('hello');
+  }
+});
